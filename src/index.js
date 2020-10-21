e4e16e7ea6273e7ba656d0380bb177470a93cfb4
Dmitry Petralai <pdo.hardwork@yandex.ru>
2020-10-21 12:56:51 +0400

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let str = n + '';
+    if (str[0] === '-') {
+        str = str.slice(1);
+    }
+    let result = '';
+    for (let i=0; i<str.length; i++) {
+        result = `${str[i]}${result}`;
+    }
+    return result;
 }
