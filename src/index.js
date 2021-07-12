c0d9fa8d56fd1783b7d98c7038db7d9636ed5e26
Dmitry_Petralai <pdo.hardwork@yandex.ru>
2021-07-12 14:04:14 +0300

    feat: add other solution according task

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 module.exports = function reverse (n) {
-    let str = n + '';
-    if (str[0] === '-') {
-        str = str.slice(1);
-    }
-    let result = '';
-    for (let i=0; i<str.length; i++) {
-        result = `${str[i]}${result}`;
-    }
-    return result;
+    // let str = n + '';
+    // if (str[0] === '-') {
+    //     str = str.slice(1);
+    // }
+    // let result = '';
+    // for (let i=0; i<str.length; i++) {
+    //     result = `${str[i]}${result}`;
+    // }
+    // return result;
+    
+    // add another solution
+    let strN = (String(n)[0] === '-') ? String(n).slice(1) : String(n);
+    return strN.split('').reverse().join('')
 }
